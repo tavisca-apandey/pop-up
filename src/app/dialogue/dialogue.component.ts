e71a329bd61445009e595848cb9a225ba93a3f6f
Tavisca-ajain <51915650+Tavisca-ajain@users.noreply.github.com>
2019-10-23 18:44:55 +0530

    Added Pop UP

---
+++ src/app/dialogue/dialogue.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialogue',
+  templateUrl: './dialogue.component.html',
+  styleUrls: ['./dialogue.component.css']
+})
+export class DialogueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
